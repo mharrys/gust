24d71c113ad4a869ab3649a84f2c25cb82991bc6
Mattias Harrysson <mathar@kth.se>
2015-01-13 18:20:32 +0100

    change vector constructor

--- src/material.cpp
+++ src/material.cpp
@@ -5,8 +5,8 @@
 gst::Material::Material(std::shared_ptr<MaterialShading> shading)
     : ambient(0.0f),
       diffuse(0.0f),
-      specular(0.0f, 0.0f, 0.0f),
-      emission(0.0f, 0.0f, 0.0f),
+      specular(0.0f),
+      emission(0.0f),
       shininess(0.0f),
       shading(shading)
 {
