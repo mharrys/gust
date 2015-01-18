1dd651bf1acf166ad9670b67f9bf32798a7023d4
Mattias Harrysson <mathar@kth.se>
2015-01-18 23:52:40 +0100

    rework pass to set render state for one render pass

--- src/pass.cpp
+++ src/pass.cpp
@@ -1,26 +1,21 @@
 #include "pass.hpp"
 
-#include "effect.hpp"
-#include "renderstate.hpp"
+#include "program.hpp"
+
+gst::Pass::Pass()
+    : Pass(nullptr)
+{
+}
 
 gst::Pass::Pass(std::shared_ptr<Program> program)
-    : program(program)
+    : blend_mode(BlendMode::NONE),
+      cull_face(CullFace::NONE),
+      depth_mask(true),
+      depth_test(false),
+      program(program)
 {
 }
 
-void gst::Pass::prepare(Effect & effect)
-{
-    effect.program = program;
-    effect.bind_callback = [this](RenderState & state) -> void {
-        this->run(state);
-        this->run();
-    };
-}
-
-void gst::Pass::run(RenderState &)
-{
-}
-
-void gst::Pass::run()
+void gst::Pass::apply(MatrixState &, LightNodes &)
 {
 }
