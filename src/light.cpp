ba969d7b780bf089fdfdcf07959af60eb179d217
Mattias Harrysson <mathar@kth.se>
2015-01-18 23:53:42 +0100

    light is now only a collection of uniforms

--- src/light.cpp
+++ src/light.cpp
@@ -1,18 +1,1 @@
 #include "light.hpp"
-
-#include "lightvisitor.hpp"
-
-gst::Light::Light()
-    : location("lights")
-{
-}
-
-gst::Light::Light(UniformArrayLocation location)
-    : location(location)
-{
-}
-
-void gst::Light::accept(LightVisitor & visitor)
-{
-    visitor.visit(*this);
-}
--- src/light.hpp
+++ src/light.hpp
@@ -1,24 +1,15 @@
 #ifndef LIGHT_HPP_INCLUDED
 #define LIGHT_HPP_INCLUDED
 
-#include "attenuation.hpp"
-#include "glm.hpp"
-#include "uniformarraylocation.hpp"
+#include "uniform.hpp"
+
+#include <vector>
 
 namespace gst
 {
-    class LightVisitor;
-    class NodeProgramUpdater;
-
     class Light {
-        friend NodeProgramUpdater;
     public:
-        Light();
-        Light(UniformArrayLocation location);
-        virtual ~Light() = default;
-        virtual void accept(LightVisitor & visitor);
-    private:
-        UniformArrayLocation location;
+        std::vector<Uniform> uniforms;
     };
 }
 
