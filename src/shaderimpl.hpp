1a0115888e57c9a8d23d310d985f8aae761daef5
Mattias Harrysson <mathar@kth.se>
2015-02-11 20:27:20 +0100

    add shader implementation

---
+++ src/shaderimpl.hpp
@@ -0,0 +1,38 @@
+#ifndef SHADERIMPL_HPP_INCLUDED
+#define SHADERIMPL_HPP_INCLUDED
+
+#include "graphicsdevice.hpp"
+#include "shader.hpp"
+
+#include <memory>
+#include <string>
+
+namespace gst
+{
+    class ProgramImpl;
+
+    class ShaderImpl : public Shader {
+        friend ProgramImpl;
+    public:
+        ShaderImpl(std::shared_ptr<GraphicsDevice> device, ShaderType type);
+        ~ShaderImpl();
+        ShaderType get_type() const;
+        // Compile specified source code.
+        void compile(std::string const & source);
+        // Return true if last compile operation on shader was successful,
+        // false otherwise.
+        bool get_compile_status() const;
+        // Return error message from last compile operation on shader.
+        std::string get_compile_error() const;
+    private:
+        ShaderHandle handle;
+        std::shared_ptr<GraphicsDevice> device;
+
+        ShaderType type;
+
+        bool compile_status;
+        std::string compile_error;
+    };
+}
+
+#endif
