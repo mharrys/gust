73320eeec3832684002a052113a3a7d7fb12be63
Mattias Harrysson <mathar@kth.se>
2015-02-11 18:33:19 +0100

    add program interface

--- src/program.hpp
+++ src/program.hpp
@@ -1,48 +1,21 @@
 #ifndef PROGRAM_HPP_INCLUDED
 #define PROGRAM_HPP_INCLUDED
 
-#include "glm.hpp"
-
-#include <memory>
-#include <string>
-#include <vector>
-#include <unordered_map>
-
 namespace gst
 {
-    class Logger;
-    class ProgramImpl;
-    class RenderState;
-    class Shader;
-    class Uniform;
-    class UniformArrayElement;
-    class UniformGroup;
+    class UniformCollection;
+    class AnnotationFormatter;
 
-    typedef std::pair<int, std::string> AttributeLocation;
-    typedef std::unordered_map<std::string, int> UniformCache;
-
+    // The responsibility of this class is to mirror a program object on the
+    // graphics card.
     class Program {
-        friend RenderState;
     public:
-        Program() = default;
-        Program(
-            std::shared_ptr<Logger> logger,
-            std::vector<Shader> shaders = {},
-            std::vector<AttributeLocation> const & locations = {});
-
-        bool operator==(Program const & other);
-        bool operator!=(Program const & other);
-        explicit operator bool() const;
-
-        void update(std::string const & name, Uniform const & uniform);
-        void update(UniformArrayElement const & element);
-        void update(UniformGroup const & group);
-    private:
-        int location(std::string const & name);
-
-        std::shared_ptr<ProgramImpl> impl;
-        std::shared_ptr<Logger> logger;
-        UniformCache uniforms;
+        // Sync specified uniforms with program object on the graphics card,
+        // every annotation is formatted with the specified formatter.
+        virtual void sync(UniformCollection const & uniforms, AnnotationFormatter const & formatter) = 0;
+    protected:
+        // Notify graphics card to install program object.
+        virtual void use() = 0;
     };
 }
 
