00510198a8a8b4b73f8a567ac69afd8ac853482b
Mattias Harrysson <mathar@kth.se>
2015-02-20 19:07:16 +0100

    buffer is now an interface

--- src/buffer.hpp
+++ src/buffer.hpp
@@ -1,14 +1,8 @@
 #ifndef BUFFER_HPP_INCLUDED
 #define BUFFER_HPP_INCLUDED
 
-#include "glm.hpp"
-#include "vertexattribute.hpp"
-
-#include <vector>
-
 namespace gst
 {
-    class BufferImpl;
     class RenderState;
 
     enum class BufferTarget {
@@ -21,37 +15,24 @@ namespace gst
         DYNAMIC
     };
 
+    // The responsibility of this class is to mirror a buffer object on the
+    // graphics card.
     class Buffer {
         friend RenderState;
     public:
-        Buffer() = default;
-        Buffer(BufferTarget target);
-
-        bool operator==(Buffer const & other);
-        bool operator!=(Buffer const & other);
-        explicit operator bool() const;
-
-        void set_usage(DataUsage usage);
-        void set_int(std::vector<int> const & data);
-        void set_uint(std::vector<unsigned int> const & data);
-        void set_float(std::vector<float> const & data);
-        void set_vec2(std::vector<glm::vec2> const & data);
-        void set_vec3(std::vector<glm::vec3> const & data);
-        void set_vec4(std::vector<glm::vec4> const & data);
-
-        VertexDataType get_type() const;
-        size_t get_count() const;
-    private:
-        bool need_new_storage(VertexDataType type, size_t count);
-        void refresh();
-
-        std::shared_ptr<BufferImpl> impl;
-        DataUsage usage;
-        VertexDataType type;
-        std::shared_ptr<void> shadowed_data;
-        size_t count;
-        size_t bytes;
-        bool dirty;
+        // Return buffer target.
+        virtual BufferTarget get_target() const = 0;
+        // Return buffer data usage.
+        virtual DataUsage get_usage() const = 0;
+        // Return buffer data size in bytes.
+        virtual unsigned int get_size_bytes() const = 0;
+        // Return the number of elements in the buffer data.
+        virtual unsigned int get_count() const = 0;
+    protected:
+        // Notify graphics card to bind this buffer.
+        virtual void bind() = 0;
+        // Sync client state with graphics card.
+        virtual void sync() = 0;
     };
 }
 
