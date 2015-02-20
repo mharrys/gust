16f3113372deb95c4bee407c937b4a44deaff2ec
Mattias Harrysson <mathar@kth.se>
2015-02-20 20:15:09 +0100

    add ability to attach index/vertex buffer to vertex array interface

--- src/vertexarray.hpp
+++ src/vertexarray.hpp
@@ -1,8 +1,14 @@
 #ifndef VERTEXARRAY_HPP_INCLUDED
 #define VERTEXARRAY_HPP_INCLUDED
 
+#include "vertexattribute.hpp"
+
+#include <memory>
+#include <vector>
+
 namespace gst
 {
+    class Buffer;
     class RenderState;
 
     // Primitives to render.
@@ -16,6 +22,9 @@ namespace gst
         TRIANGLES
     };
 
+    typedef std::vector<VertexAttribute> VertexAttributes;
+    typedef std::pair<std::shared_ptr<Buffer>, VertexAttributes> VertexBuffer;
+
     // The responsibility of this class is to mirror a vertex array object on
     // the graphics card.
     class VertexArray {
@@ -25,6 +34,10 @@ namespace gst
         virtual void draw_arrays(DrawMode mode, int first, int count) = 0;
         // Render primitives from stored array data.
         virtual void draw_elements(DrawMode mode, int count) = 0;
+        // Set and enable index buffer.
+        virtual void set_index_buffer(std::shared_ptr<Buffer> buffer) = 0;
+        // Add and enable vertex buffer.
+        virtual void add_vertex_buffer(std::shared_ptr<Buffer> buffer, VertexAttributes attributes) = 0;
     protected:
         // Notify graphics card to bind this vertex array.
         virtual void bind() = 0;
