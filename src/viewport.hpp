d7b35bb65e662bd22c85fd7558fc6b3d96244c71
Mattias Harrysson <mathar@kth.se>
2015-02-22 20:17:07 +0100

    add viewport documentation

--- src/viewport.hpp
+++ src/viewport.hpp
@@ -5,6 +5,7 @@ namespace gst
 {
     class Resolution;
 
+    // The responsibility of this class is to describe a viewport shape.
     class Viewport {
     public:
         Viewport();
@@ -12,12 +13,20 @@ namespace gst
         Viewport(unsigned int width, unsigned int height);
         Viewport(int x, int y, unsigned int width, unsigned int height);
 
+        // Return true if specified viewport is equal to this viewport, false
+        // otherwise.
         bool operator==(Viewport const & other) const;
+        // Return true if specified viewport is not equal to this viewport,
+        // false otherwise.
         bool operator!=(Viewport const & other) const;
 
+        // Return x component.
         int get_x() const;
+        // Return y component.
         int get_y() const;
+        // Return width component.
         int get_width() const;
+        // Return height component.
         int get_height() const;
     private:
         int x;
