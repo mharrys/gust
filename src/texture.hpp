a037fb03b03b379fe7dd86e43e89fcf76bc61dca
Mattias Harrysson <mathar@kth.se>
2015-02-22 17:35:06 +0100

    add texture documentation

--- src/texture.hpp
+++ src/texture.hpp
@@ -3,7 +3,6 @@
 
 #include "graphicsdevice.hpp"
 
-#include <memory>
 #include <vector>
 
 namespace gst
@@ -12,16 +11,25 @@ namespace gst
     class RenderState;
     class Resolution;
 
+    // The responsibility of this class is to mirror a texture object on the
+    // graphics card.
     class Texture {
         friend RenderState;
     public:
+        // Set storage size.
         virtual void set_size(Resolution size) = 0;
+        // Set storage data.
         virtual void set_data(std::vector<unsigned char> const & data) = 0;
+        // Set storage size and data.
         virtual void set_image(Image const & image) = 0;
+        // Set storage parameters.
         virtual void set_param(TextureParam const & param) = 0;
+        // Return identifier on the graphics card.
         virtual TextureHandle get_handle() const = 0;
     protected:
+        // Notify graphics card to bind this texture.
         virtual void bind(int unit) = 0;
+        // Sync client state with graphics card.
         virtual void sync() = 0;
     };
 }
