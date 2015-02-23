1c12f9511083641d4b533bb77c5cf4d21b2b9911
Mattias Harrysson <mathar@kth.se>
2015-02-23 21:08:37 +0100

    add documentation to node visitor

--- src/nodevisitor.hpp
+++ src/nodevisitor.hpp
@@ -9,13 +9,20 @@ namespace gst
     class ModelNode;
     class Node;
 
+    // The responsibility of this class is perform operations on a node
+    // structure (visitor pattern).
     class NodeVisitor {
     public:
         virtual ~NodeVisitor() = default;
+        // Visit a camera node.
         virtual void visit(CameraNode & node);
+        // Visit a group node.
         virtual void visit(GroupNode & node);
+        // Visit a light node.
         virtual void visit(LightNode & node);
+        // Visit a model node.
         virtual void visit(ModelNode & node);
+        // Visit a abstract node.
         virtual void visit(Node & node);
     };
 }
