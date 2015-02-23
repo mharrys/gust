1e477df4e48665dd57c73e09f1da3b1e910b6d80
Mattias Harrysson <mathar@kth.se>
2015-02-23 20:25:42 +0100

    add documentation to spatial

--- src/spatial.hpp
+++ src/spatial.hpp
@@ -5,18 +5,28 @@
 
 namespace gst
 {
+    // The responsibility of this class is to describe a position and
+    // orientation in Euclidean space.
     class Spatial {
     public:
         Spatial();
 
+        // Translate distance on axis in current orientation.
         void translate(float distance, glm::vec3 axis);
+        // Translate distance on x-axis in current orientation.
         void translate_x(float distance);
+        // Translate distance on y-axis in current orientation.
         void translate_y(float distance);
+        // Translate distance on z-axis in current orientation.
         void translate_z(float distance);
 
+        // Rotate with specified number of degrees around specified axis.
         void rotate(float angle_deg, glm::vec3 axis);
+        // Rotate with specified number of degrees around x-axis.
         void rotate_x(float angle_deg);
+        // Rotate with specified number of degrees around y-axis.
         void rotate_y(float angle_deg);
+        // Rotate with specified number of degrees around z-axis.
         void rotate_z(float angle_deg);
 
         glm::vec3 position;
