498e2a912509594dd27fb1e9f63c650ebf43a9e9
Mattias Harrysson <mathar@kth.se>
2015-02-23 19:56:24 +0100

    add documentation to resolution

--- src/resolution.hpp
+++ src/resolution.hpp
@@ -3,16 +3,26 @@
 
 namespace gst
 {
+    // The responsibility of this class is to describe a resolution.
     class Resolution {
     public:
+        // Construct resolution with 0 width and height.
         Resolution();
+        // Construct resolution with equal width and height.
         Resolution(unsigned int size);
+        // Construct resolution with specified with and height.
         Resolution(unsigned int width, unsigned int height);
 
+        // Return true if specified resolution is equal to this resolution,
+        // false otherwise.
         bool operator==(Resolution const & other);
+        // Return true if specified resolution is not equal to this viewport,
+        // false otherwise.
         bool operator!=(Resolution const & other);
 
+        // Return width.
         unsigned int get_width() const;
+        // Return height.
         unsigned int get_height() const;
     private:
         unsigned int width;
