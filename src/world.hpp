f15711da47a95fcc8d52384f0ae55e88760bd43a
Mattias Harrysson <mathar@kth.se>
2015-02-23 20:52:16 +0100

    world is now a interface

--- src/world.hpp
+++ src/world.hpp
@@ -5,12 +5,19 @@
 
 namespace gst
 {
+    // The responsibility of this class is to structure callback for running a
+    // main event loop for a open window.
     class World {
     public:
         virtual ~World() = default;
+        // Called once before entering main event loop. Return true if
+        // initialization was successful, false otherwise.
         virtual bool create() = 0;
-        virtual void update(seconds delta, seconds elapsed);
-        virtual void destroy();
+        // Called until window is closed after initialization is successful.
+        virtual void update(seconds delta, seconds elapsed) = 0;
+        // Called once after main program event loop exit its control, will be
+        // called regardless if initialization was successful or not.
+        virtual void destroy() = 0;
     };
 }
 
