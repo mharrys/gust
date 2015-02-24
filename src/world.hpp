2eb32506daa84982641ea3a20372e18dc3fd9ee3
Mattias Harrysson <mathar@kth.se>
2015-02-24 21:46:10 +0100

    adjust world documentation

--- src/world.hpp
+++ src/world.hpp
@@ -6,20 +6,19 @@
 namespace gst
 {
     // The responsibility of this class is to structure callback for running a
-    // main event loop for a open window.
+    // main event loop.
     class World {
     public:
         virtual ~World() = default;
         // Called once before entering main event loop. Return true if
         // initialization was successful, false otherwise.
         virtual bool create() = 0;
-        // Called until window is closed after initialization is successful.
-        // The delta parameter represents the time in seconds since the last
-        // update and the elapsed parameter represents the time in seconds
-        // since create was called.
+        // Called until window is closed if initialization was successful,
+        // 'delta' is seconds since last update and 'elapsed' is time since
+        // initialization.
         virtual void update(float delta, float elapsed) = 0;
-        // Called once after main program event loop exit its control, will be
-        // called regardless if initialization was successful or not.
+        // Called once after main event loop exit its control, will be called
+        // regardless if initialization was successful or not.
         virtual void destroy() = 0;
     };
 }
