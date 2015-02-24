6a901437a216adf6b11120874fb1d4184be34b43
Mattias Harrysson <mathar@kth.se>
2015-02-24 21:44:30 +0100

    add documentation to world runner

--- src/worldrunner.hpp
+++ src/worldrunner.hpp
@@ -7,8 +7,12 @@ namespace gst
     class Window;
     class World;
 
+    // The responsibility of this class is to control the main event loop for
+    // a world.
     class WorldRunner {
     public:
+        // Control main event loop. Return 0 if exit was successful, 1
+        // otherwise.
         int control(World & world, Clock & clock, Window & window) const;
     };
 }
