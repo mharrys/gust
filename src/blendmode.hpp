b55ded12828a87d619bb586ecb0432aa2b44ede2
Mattias Harrysson <mathar@kth.se>
2015-03-14 19:58:03 +0100

    fix redundant blend mode description

--- src/blendmode.hpp
+++ src/blendmode.hpp
@@ -6,9 +6,9 @@ namespace gst
     // Supported blending techniques.
     enum class BlendMode {
         NONE,           // Blending disabled.
-        ADDITIVE,       // Additive blending: out = src_frag + dst_frag.
-        MULTIPLICATIVE, // Multiplicative blending: out = src_frag * dst_frag.
-        INTERPOLATIVE   // Interpolative blending: out = (src_alpha * src_frag) + ((1 - src_alpha) * dst_frag).
+        ADDITIVE,       // out = src_frag + dst_frag.
+        MULTIPLICATIVE, // out = src_frag * dst_frag.
+        INTERPOLATIVE   // out = (src_alpha * src_frag) + ((1 - src_alpha) * dst_frag).
     };
 }
 
