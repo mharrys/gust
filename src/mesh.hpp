4172b41794234c891d6d16f3525c7e40dd8d5fec
Mattias Harrysson <mathar@kth.se>
2015-03-21 22:20:41 +0100

    add documentation to mesh

--- src/mesh.hpp
+++ src/mesh.hpp
@@ -16,16 +16,24 @@ namespace gst
     class VertexArray;
     class VertexBuffer;
 
+    // The responsibility of this class is to describe a mesh, a collection of
+    // vertices.
     class Mesh {
         friend NodeRenderer;
     public:
         Mesh() = default;
         Mesh(std::shared_ptr<VertexArray> vertex_array);
+        // Set draw mode.
         void set_draw_mode(DrawMode mode);
+        // Set positions from data.
         void set_positions(std::vector<glm::vec3> const & data);
+        // Set normals from data (optional).
         void set_normals(std::vector<glm::vec3> const & data);
+        // Set colors from data (optional).
         void set_colors(std::vector<glm::vec3> const & data);
+        // Set texture coordinates from data (optional).
         void set_tex_coords(std::vector<glm::vec2> const & data);
+        // Set indices from data (optional).
         void set_indices(std::vector<unsigned int> const & data);
     private:
         std::shared_ptr<VertexArray> vertex_array;
