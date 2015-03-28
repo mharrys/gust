e270bf8f6f65b3e8208a2377f6c37709f0169317
Mattias Harrysson <mathar@kth.se>
2015-03-28 20:06:14 +0100

    add more documentation to light

--- src/light.hpp
+++ src/light.hpp
@@ -13,18 +13,23 @@ namespace gst
     // collection of uniforms.
     class Light {
     public:
+        // Construct light with a default uniform implementation using a
+        // annotation formatter for free uniforms.
         static Light create_free();
+        // Construct light with a default uniform implementation using a
+        // annotation formatter for uniforms in a struct.
         static Light create_struct(std::string const & name);
+        // Construct light with a default uniform implementation using a
+        // annotation formatter for uniforms in a array.
         static Light create_array(std::string const & name);
-
+        // Construct empty light.
         Light() = default;
+        // Construct light from uniforms.
         Light(std::shared_ptr<UniformMap> uniforms);
-
         // Set light enabled/disabled.
         void set_enabled(bool enabled);
         // Return true if enabled, false otherwise.
         bool get_enabled() const;
-
         // Return uniform.
         ShadowedData & get_uniform(std::string const & annotation);
         // Return all uniforms that describe this light.
